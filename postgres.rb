2be95d78c7d6ef34553f20b9563fb95df20e9a0e
Thomas <thomas@icdesign.com.au>
2010-12-16 13:57:43 +0800

    Switched around order of meet and met?

--- postgres.rb
+++ postgres.rb
@@ -1,10 +1,11 @@
-dep "postgres9" do
-  met? {
-    which "psql"
-  }
-  
+dep "postgres9" do  
   meet {
     source 'http://wwwmaster.postgresql.org/redir/311/h/source/v9.0.2/postgresql-9.0.2.tar.gz'
     provides 'psql'
   }
+  
+  met? {
+    which "psql"
+  }
+  
 end
