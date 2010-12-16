e47db7bef08f538a82273f0882ff1f656c418641
Thomas <thomas@icdesign.com.au>
2010-12-16 14:10:11 +0800

    Removed met method

--- postgres.rb
+++ postgres.rb
@@ -1,9 +1,7 @@
-dep "postgres9" do  
-  meet {
-    source 'http://wwwmaster.postgresql.org/redir/311/h/source/v9.0.2/postgresql-9.0.2.tar.gz'
-    provides 'psql'
-  }
-  
+dep "postgres9" do
+  provides "psql"
+  source "http://wwwmaster.postgresql.org/redir/311/h/source/v9.0.2/postgresql-9.0.2.tar.gz"
+
   met? {
     which "psql"
   }
