8be82ec0371f138ba9c550fdf07ca45a77424807
Thomas <thomas@icdesign.com.au>
2010-12-16 13:50:48 +0800

    Updated postgres ti includ a met?

--- postgres.rb
+++ postgres.rb
@@ -1,7 +1,9 @@
 dep "postgres9" do
-  source 'http://wwwmaster.postgresql.org/redir/311/h/source/v9.0.2/postgresql-9.0.2.tar.gz'
-  provides 'psql'
-  process_source {
-    sudo "echo test" 
+  met? {
+    which { psql }
+  }
+  
+  meet {
+    
   }
 end
